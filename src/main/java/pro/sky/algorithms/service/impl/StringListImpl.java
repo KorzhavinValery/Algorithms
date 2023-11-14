eb517a99cad8675e90597861f6c7e604c23285fc
Vel <korzhavin.vel@yandex.ru>
2023-11-14 11:11:27 +0700

    Algorithms, реализовал String add

--- src/main/java/pro/sky/algorithms/service/impl/StringListImpl.java
+++ src/main/java/pro/sky/algorithms/service/impl/StringListImpl.java
@@ -1,17 +1,39 @@
 package pro.sky.algorithms.service.impl;
 
+import pro.sky.algorithms.exceptions.InvalidIndexException;
+import pro.sky.algorithms.exceptions.NullItemException;
+import pro.sky.algorithms.exceptions.StorageIsFullException;
 import pro.sky.algorithms.service.StringList;
 
+import java.util.Arrays;
+
 public class StringListImpl implements StringList {
+ private  final String [] stringList;
+ private int size;
+
+    public StringListImpl() {
+        stringList = new String[12];
+    }
 
     @Override
     public String add(String item) {
-        return null;
+        checkSize();
+        checkItem(item);
+        stringList[size++] = item;
+        return item;
     }
 
     @Override
     public String add(int index, String item) {
-        return null;
+        checkSize();
+        checkIndex(index);
+        checkItem(item);
+        if (index == size) {
+            stringList[size++] = item;
+            return item;
+        }
+        System.arraycopy(stringList, index, stringList, index + 1, size - index);
+        return  item;
     }
 
     @Override
@@ -51,26 +73,44 @@ public class StringListImpl implements StringList {
 
     @Override
     public boolean equals(StringList otherList) {
-        return false;
+        return Arrays.equals(this.toArray(), otherList.toArray());
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public void clear() {
-
+        size = 0;
     }
 
     @Override
     public String[] toArray() {
-        return new String[0];
+        return Arrays.copyOf(stringList, size);
+    }
+
+    private void checkSize() {
+        if (size == stringList.length) {
+            throw new StorageIsFullException();
+        }
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index > size) {
+            throw new InvalidIndexException();
+        }
+    }
+
+    private void checkItem(String item) {
+        if (item == null) {
+            throw new NullItemException();
+        }
     }
 }
