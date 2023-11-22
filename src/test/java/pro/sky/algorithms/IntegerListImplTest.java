9668449c1405b69966408c2f61b4465b380a2b47
Vel <korzhavin.vel@yandex.ru>
2023-11-22 14:52:01 +0700

    Algorithms, 3 часть выполнено

--- src/test/java/pro/sky/algorithms/IntegerListImplTest.java
+++ src/test/java/pro/sky/algorithms/IntegerListImplTest.java
@@ -9,10 +9,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class IntegerListImplTest {
     IntegerListImpl out = new IntegerListImpl(1);
+    IntegerListImpl out1 = new IntegerListImpl(1);
 
     @BeforeEach
     public void setUp() {
         out.add(1);
+        out1.add(-15);
+        out1.add(0);
+        out1.add(2);
+        out1.add(6);
+        out1.add(5);
+        out1.add(5);
+        out1.add(20);
+        out1.add(28);
+        out1.add(70);
+        out1.add(57);
 
 
     }
@@ -27,7 +38,7 @@ public class IntegerListImplTest {
     void shouldAddByIndex() {
         out.add(0, 2);
         out.add(1, 7);
-        Integer[] expected = {2,7,1};
+        Integer[] expected = {2, 7, 1};
         assertArrayEquals(expected, out.toArray());
     }
 
@@ -54,7 +65,38 @@ public class IntegerListImplTest {
 
     @Test
     void shouldItemNotFoundException() {
-        assertThrows(ItemNotFoundException.class, ()->out.remove(1645456));
+        assertThrows(ItemNotFoundException.class, () -> out.remove(1645456));
     }
 
+    Integer[] expectedForSortTest = {-15, 0, 2, 5, 5, 6, 20, 28, 57, 70};
+
+    @Test
+    void sortBubbleTest() {
+        out1.sortBubble();
+        assertArrayEquals(expectedForSortTest, out1.toArray());
+    }
+
+    @Test
+    void sortSelectionTest() {
+        out1.sortSelection();
+        assertArrayEquals(expectedForSortTest, out1.toArray());
+    }
+
+    @Test
+    void sortInsertionTest() {
+        out1.sortInsertion();
+        assertArrayEquals(expectedForSortTest, out1.toArray());
+    }
+
+    @Test
+    void sortMergeTest() {
+        out1.sortMerge();
+        assertArrayEquals(expectedForSortTest, out1.toArray());
+    }
+
+    @Test
+    void sortQuickTest() {
+        out1.sortQuick();
+        assertArrayEquals(expectedForSortTest, out1.toArray());
+    }
 }
